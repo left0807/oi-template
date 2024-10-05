73edbd3304b6c371c7426012edf52984459c8418
kotnid <86507779+kotnid@users.noreply.github.com>
2024-10-06 01:51:34 +0800

    fix : Modular Inverse precompute too slow

--- maths/ModularInverse.cpp
+++ maths/ModularInverse.cpp
@@ -1,41 +1,36 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 
-const int MOD = 1e9 + 7;
+typedef long long ll;
+const ll MOD = 998244353; // bruh
 
-int pow(int x, int y){
-    int res = 1;
+ll pow(ll x, ll y){
+    ll res = 1;
     x = x%MOD;
-
     while(y > 0){
         if(y&1) res = (res*x)%MOD;
-
         y = y >> 1;
         x = (x*x)%MOD;
     }
     return res;
 }
 
-int inv(int x){
+ll inv(ll x){
     return pow(x, MOD-2);
 }
 
-vector<int> fact{1}, invfact{1};
+const int N = 1e7+1;
+vector<ll> fact(N), invfact(N);
 
-int C(int n, int r){
+ll C(ll n, ll r){
     if(n < r) return 0;
-
-    for(int i = fact.size(); i <= n; i++){
-        fact.push_back((fact.back()*i)%MOD);
-        invfact.push_back(inv(fact.back()));
-    }
-
     return ((fact[n]*invfact[r])%MOD*invfact[n-r])%MOD;
 }
 
-int32_t main() 
-{
-    cout << C(4, 2) << '\n';
-    return 0; 
-} 
+void precompute(){
+    fact[0] = invfact[0] = 1;
+    for(ll i = 1; i<N; i++)fact[i] = (fact[i-1]*i)%MOD;
+    invfact[N-1] = inv(fact[N-1]);
+    for(ll i=N-2; i>=1; i--)invfact[i] = invfact[i+1]*(i+1)%MOD;
+    
+}
