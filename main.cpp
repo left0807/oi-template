2d4291a991acb843a160a6d9f75394fe42961f81
Left0807 <matthewchor9810@gmail.com>
2025-10-25 00:05:15 +0800

    fix: main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #define all(a) a.begin(), a.end()
 #define sz(a) (int)a.size()
 #define int long long
-#define Tp template<typename T>
-#define Ts template<typename T,typename... Ar>
+#define Tp template <typename T>
+#define Ts template <typename T, typename... Ar>
 using namespace std;
 
 template <class T>
@@ -34,41 +34,61 @@ istream &operator>>(istream &is, vector<T> &data)
                 is >> x;
         return is;
 }
- 
-template<class T>inline void chmin(T &x, T y) { if (y < x) x = y; }
-template<class T>inline void chmax(T &x, T y) { if (y > x) x = y; }
-namespace Debug{
-	Tp void _debug(char* f,T t){cerr<<f<<'='<<t<<endl;}
-	Ts void _debug(char* f,T x,Ar... y){while(*f!=',') cerr<<*f++;cerr<<'='<<x<<",";_debug(f+1,y...);}
-	#ifdef LOCAL
-	#define err(...) _debug((char*)#__VA_ARGS__,__VA_ARGS__)
-	#else 
-	#define err(...) void()
-	#endif
-}using namespace Debug;
+
+template <class T>
+inline void chmin(T &x, T y)
+{
+        if (y < x)
+                x = y;
+}
+template <class T>
+inline void chmax(T &x, T y)
+{
+        if (y > x)
+                x = y;
+}
+namespace Debug
+{
+        Tp void _debug(char *f, T t) { cerr << f << '=' << t << endl; }
+        Ts void _debug(char *f, T x, Ar... y)
+        {
+                while (*f != ',')
+                        cerr << *f++;
+                cerr << '=' << x << ",";
+                _debug(f + 1, y...);
+        }
+#ifdef LOCAL
+#define err(...) _debug((char *)#__VA_ARGS__, __VA_ARGS__)
+#else
+#define err(...) void()
+#endif
+}
+using namespace Debug;
 
 mt19937_64 rng(chrono::steady_clock::time_point::clock().now().time_since_epoch().count());
 
 const int N = 5e5 + 10;
 const int INF = 1e18;
-const int MOD = 998244353; 
+const int MOD = 998244353;
 const int LOG = 20;
 
 typedef __int128_t ll;
 
-int solve(){
+int solve()
+{
 }
 
 int32_t main()
 {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
+        ios::sync_with_stdio(false);
+        cin.tie(0);
 
-  int tt;
-  cin >> tt;
-  for(int i = 1; i <= tt; i++){
-	printf("Case #%d: %d\n", i, solve());
-  }
+        int tt;
+        cin >> tt;
+        for (int i = 1; i <= tt; i++)
+        {
+                printf("Case #%d: %d\n", i, solve());
+        }
 }
 
 // 0 2 1 -> 1 1 0 0 0
