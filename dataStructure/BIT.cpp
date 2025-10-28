cc40cef0a649279d2cc6d86c9fd26ffdde0ef703
Left0807 <matthewchor9810@gmail.com>
2025-10-28 22:53:41 +0800

    feat: update BIT, add range_add() and kth() func

--- dataStructure/BIT.cpp
+++ dataStructure/BIT.cpp
@@ -1,44 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int N = 1e6 + 10;
 
-int BIT[1100][1100];
-int s;
-
-void init(int S) {
-  for(int i=0 ; i<=S ; i++)
-    for(int j=0 ; j<=S ; j++)
-      BIT[i][j] = 0;
-
-  s = S;
+int BIT[N];
+void add(int x, int v){
+    for(int i = x; i < N; i += i&-i) BIT[i] += v;
+}
+int qry(int x){
+    int res = 0; for(int i = x; i > 0; i -= i&-i) res += BIT[i]; return res;
 }
 
-// update BIT
-void update(int i , int j , int v){
-  i++;
-  j++;
-  for( ; i<=s ; i += (i&-i)){ // row
-    for(int k =j ; k<=s ; k+=(k&-k)){ // column
-      BIT[i][k] += v; // update value
+int kth(int k){
+    int sum = 0, x = 0;
+    for(int i = __lg(N); i >= 0; i--){
+        x += 1 << i;
+        if(x >= N || sum + BIT[x] >= k)
+            x -= 1 << i;
+        else
+            sum += BIT[x];
     }
-  }
+    return x + 1;
 }
 
-// query BIT
-int que(int i , int j){
-  if(i <=0 || j <= 0 )return 0; // avoid runtime err
-  int res = 0;
-
-  for( ; i ; i-=(i&-i)){ // row 
-    for(int k = j ; k ; k-=(k&-k)){ // column
-      res += BIT[i][k]; // val
-    }
-  }
-  
-  return res;
+// BIT range add, range query
+int t1[N], t2[N];
+void add(int x, int v){
+    int v1 = x * v;
+    for(int i = x; i < N; i += i&-i) t1[i] += v, t2[i] += v1;
 }
 
-// sum of range
-int sum(int i , int j , int x , int y){
-  i++;j++;x++;y++;
-  return que(x,y) - que(x , j-1) - que(i-1 , y) + que(i-1 , j-1); // 2d prefix sum
+int getsum(int *t, int x){
+    int res = 0; for(int i = x; x > 0; i -= i&-i) res += t[i]; return res;
+}
+
+void range_add(int l, int r, int v){
+    add(l, v), add(r + 1, -v);
+}
+
+int range_sum(int l, int r) {
+  return (r + 1ll) * getsum(t1, r) - 1ll * l * getsum(t1, l - 1) -
+         (getsum(t2, r) - getsum(t2, l - 1));
 }
